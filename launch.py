ad701fe02cb02320c514f5d57ea280c3c8ac6a84
Hplayfree25 <109486002+Hplayfree25@users.noreply.github.com>
2023-10-12 14:39:37 +0700

    Update launch.py

--- launch.py
+++ launch.py
@@ -2,9 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='exnavy',
-    version='0.0.1',
-    description='Paket ini dirancang untuk memastikan operasi Stable Diffusion yang mulus di berbagai platform notebook.',
-    author='Revaldo',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'safetensors==0.3.1',
@@ -17,13 +15,8 @@ setup(
         'xmltodict==0.13.0',
         'pydantic==1.10.9'
     ],
+    author='Revaldo',
     author_email='revaldoanjennurillifanderson@gmail.com',
+    description='A utility library for Other Notebook',
     url='https://github.com/Hplayfree25/exnavy',
-    keywords='ExNavy',
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3.8',
-    ],
 )
